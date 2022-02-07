04c21dede2711be7d7098d52a0b4615023d96f85
ArturZaczek <76411689+ArturZaczek@users.noreply.github.com>
2022-02-07 23:19:32 +0100

    changed variable names in file
    changed variable names in file

--- modifier.py
+++ modifier.py
@@ -1,20 +1,19 @@
-fileName = "XXXVI Nadzwyczajna Sesja Rady Miejskiej w Zatorze 07 09 2021"
+fileName = "file name in the same directory"
 
-file = open( "przed dodaniem\\"+ fileName +".srt", "r", encoding="utf-8")
-file1 = open( "po dodaniu tekstu maszynowego\\"+ fileName +"_dodano_tekst_maszynowy.srt", "w", encoding="utf-8")
+inputFile = open( "input path to the directory\\"+ fileName +".srt", "r", encoding="utf-8")
+outputFile = open( "output path to the directory\\"+ fileName +"_second part of the output name.srt", "w", encoding="utf-8")
 
-tak = file.readlines()
-d = 0
-c = 0
-for i in tak:
-    d = d+1
+inputFileLines = inputFile.readlines()
+lineNumber = 0
+lineBuffor = 0
+for i in inputFileLines:
+    lineNumber = lineNumber+1
     if i[0] == "0" or i[0] == "1" or i[0] == "2" or i[0] == "3" or i[0] == "4" or i[0] == "5" or i[0] == "6" or i[0] == "7" or i[0] == "8" or i[0] == "9" or i[0] == " " or i[0] == "":
-        # f = f+1
-        file1.write(i)
+        outputFile.write(i)
     else:
-        if c+1 == d or c+2 == d :
-            c = d
-            file1.write(i)
+        if lineBuffor+1 == lineNumber or lineBuffor+2 == lineNumber :
+            lineBuffor = lineNumber
+            outputFile.write(i)
         else:
-            c = d
-            file1.write("[Tłumaczenie  Maszynowe]: " + i)
+            lineBuffor = lineNumber
+            outputFile.write("[text in every first line of text, in file]: " + i)
