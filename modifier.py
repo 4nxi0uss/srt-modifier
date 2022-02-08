d2230f8edb9f56571e25bc4e9b7a5b4d5b55a430
ArturZaczek <76411689+ArturZaczek@users.noreply.github.com>
2022-02-08 15:57:55 +0100

    added ability to modify multiple files
    added ability to modify multiple files, but still from code not from console

--- modifier.py
+++ modifier.py
@@ -1,19 +1,34 @@
-fileName = "file name in the same directory"
+import glob
+import os
 
-inputFile = open( "input path to the directory\\"+ fileName +".srt", "r", encoding="utf-8")
-outputFile = open( "output path to the directory\\"+ fileName +"_second part of the output name.srt", "w", encoding="utf-8")
+file_location = os.path.join('C:\\','path','to','input','directory','*.srt')
 
-inputFileLines = inputFile.readlines()
-lineNumber = 0
-lineBuffor = 0
-for i in inputFileLines:
-    lineNumber = lineNumber+1
-    if i[0] == "0" or i[0] == "1" or i[0] == "2" or i[0] == "3" or i[0] == "4" or i[0] == "5" or i[0] == "6" or i[0] == "7" or i[0] == "8" or i[0] == "9" or i[0] == " " or i[0] == "":
-        outputFile.write(i)
-    else:
-        if lineBuffor+1 == lineNumber or lineBuffor+2 == lineNumber :
-            lineBuffor = lineNumber
+filenames = glob.glob(file_location)
+
+for file in filenames :
+
+    inputFile = open( file , "r", encoding="utf-8")
+    outputFile = open( file_location.split("*")[0]+"output Directory\\" + file.removeprefix(file_location.split("*")[0]).removesuffix(".srt") +"_second part of the output name.srt", "w", encoding="utf-8")
+
+    inputFileLines = inputFile.readlines()
+    
+    lineNumber = 0
+    lineBuffor = 0
+    
+    for i in inputFileLines:
+        
+        lineNumber = lineNumber+1
+        
+        if i[0] == "0" or i[0] == "1" or i[0] == "2" or i[0] == "3" or i[0] == "4" or i[0] == "5" or i[0] == "6" or i[0] == "7" or i[0] == "8" or i[0] == "9" or i[0] == " " or i[0] == "":
             outputFile.write(i)
+            
+        elif lineBuffor+1 == lineNumber or lineBuffor+2 == lineNumber :
+                lineBuffor = lineNumber
+                outputFile.write(i)
+                
         else:
-            lineBuffor = lineNumber
-            outputFile.write("[text in every first line of text, in file]: " + i)
+                lineBuffor = lineNumber
+                outputFile.write("[text in first line of text in every block of text]: " + i)
+                
+    inputFile.close()
+    outputFile.close()
