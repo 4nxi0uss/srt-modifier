babee07fca130b834f795e0e7127306c1068328a
ArturZaczek <76411689+ArturZaczek@users.noreply.github.com>
2022-02-08 23:25:54 +0100

    changed from camelCase to snake_case

--- modifier.py
+++ modifier.py
@@ -3,32 +3,32 @@ import os
 
 file_location = os.path.join('C:\\','path','to','input','directory','*.srt')
 
-filenames = glob.glob(file_location)
+file_names = glob.glob(file_location)
 
-for file in filenames :
+for file in file_names :
 
-    inputFile = open( file , "r", encoding="utf-8")
-    outputFile = open( file_location.split("*")[0]+"output Directory\\" + file.removeprefix(file_location.split("*")[0]).removesuffix(".srt") +"_second part of the output name.srt", "w", encoding="utf-8")
+    input_file = open( file , "r", encoding="utf-8")
+    output_file = open( file_location.split("*")[0]+"output Directory\\" + file.removeprefix(file_location.split("*")[0]).removesuffix(".srt") +"_second part of the output name.srt", "w", encoding="utf-8")
 
-    inputFileLines = inputFile.readlines()
+    input_file_lines = input_file.readlines()
     
-    lineNumber = 0
-    lineBuffor = 0
+    line_number = 0
+    line_buffor = 0
     
-    for i in inputFileLines:
+    for i in input_file_lines:
         
-        lineNumber = lineNumber+1
+        line_number = line_number+1
         
         if i[0] == "0" or i[0] == "1" or i[0] == "2" or i[0] == "3" or i[0] == "4" or i[0] == "5" or i[0] == "6" or i[0] == "7" or i[0] == "8" or i[0] == "9" or i[0] == " " or i[0] == "":
-            outputFile.write(i)
+            output_file.write(i)
             
-        elif lineBuffor+1 == lineNumber or lineBuffor+2 == lineNumber :
-                lineBuffor = lineNumber
-                outputFile.write(i)
+        elif line_buffor+1 == line_number or line_buffor+2 == line_number :
+                line_buffor = line_number
+                output_file.write(i)
                 
         else:
-                lineBuffor = lineNumber
-                outputFile.write("[text in first line of text in every block of text]: " + i)
+                line_buffor = line_number
+                output_file.write("[text in first line of text in every block of text]: " + i)
                 
-    inputFile.close()
-    outputFile.close()
+    input_file.close()
+    output_file.close()
